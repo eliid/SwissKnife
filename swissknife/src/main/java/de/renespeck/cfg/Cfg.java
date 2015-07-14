4c96d42b880090f2a9d5e67557e27c1919d0ebf0
renespeck <speckrene@gmail.com>
2015-07-14 14:58:46 +0200

    Backup.

---
+++ swissknife/src/main/java/de/renespeck/cfg/Cfg.java
@@ -0,0 +1,11 @@
+package de.renespeck.cfg;
+
+import java.io.File;
+
+public class Cfg {
+
+    public static String CFG_FILE   = "default";
+    public static String CFG_FOLDER = "config";
+    public static String LOG_FILE   = CFG_FOLDER + File.separator + "log4j.properties";
+
+}
