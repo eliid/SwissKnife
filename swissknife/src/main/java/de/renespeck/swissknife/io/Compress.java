3b6bb7c245761576180f13fbce2d61e7075ba6f6
renespeck <speckrene@gmail.com>
2015-07-22 22:42:37 +0200

    Adds Compress class.

---
+++ swissknife/src/main/java/de/renespeck/swissknife/io/Compress.java
@@ -0,0 +1,33 @@
+package de.renespeck.swissknife.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import edu.stanford.nlp.io.StringOutputStream;
+
+public class Compress {
+    public static final Logger LOG = LogManager.getLogger(Compress.class);
+
+    public static String gunzipIt(String zipFile) {
+        byte[] buffer = new byte[1024];
+        StringOutputStream sos = new StringOutputStream();
+        try {
+            GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(zipFile));
+            int len;
+            while ((len = gzis.read(buffer)) > 0) {
+                sos.write(buffer, 0, len);
+            }
+
+            gzis.close();
+            sos.close();
+
+        } catch (IOException e) {
+            LOG.error(e.getLocalizedMessage(), e);
+        }
+        return sos.toString();
+    }
+}
